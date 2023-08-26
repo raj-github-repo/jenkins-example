c8fa9e742072925239d8a57df11ffe96c118bc36
raj-github-repo <141130941+raj-github-repo@users.noreply.github.com>
2023-08-26 22:16:27 +0530

    new commit in fizzjava

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -8,7 +8,7 @@ public class FizzBuzz {
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
-        // comment
+        
         return String.valueOf(number);
     }
 }
